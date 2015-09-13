ea08d9f872fe3470564f779c71c5759f80bea085
qiujoe <qiujinglei@gmail.com>
2015-09-13 20:22:37 +0800

    add log

---
+++ StockCommon/LogHelper.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * This library is part of OpenCms -
+ * the Open Source Content Management System
+ *
+ * Copyright (c) qiujoe (http://www.github.com/qiujoe)
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * For further information about StockTrader, please see the
+ * project website: http://www.github.com/qiujoe/StockTrader
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stock.Common
+{
+    class LogHelper
+    {
+        public static void WriteLog(Type t, String message)
+        {
+            Console.WriteLine(t.ToString() + ": " + message);
+        }
+
+        public static void WriteErrorLog(Type t, String message, Exception ex)
+        {
+            Console.WriteLine(t.ToString() + ": " + message);
+            Console.WriteLine(ex.StackTrace);
+        }
+    }
+}
