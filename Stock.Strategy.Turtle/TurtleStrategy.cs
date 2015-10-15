a2cbbff151678eab6a9b4597bd673fed29c1c363
qiujoe <qiujinglei@gmail.com>
2015-10-15 09:27:14 +0800

    增加海龟策略

---
+++ Stock.Strategy.Turtle/TurtleStrategy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stock.Strategy.Turtle
+{
+    public class TurtleStrategy : BaseStrategy
+    {
+        public TurtleStrategy()
+        {
+            IsValid = false;
+        }
+
+        public override void OnTicket(object sender)
+        {
+            
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return " 海龟交易法则"; 
+            }
+            set{ }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "海龟交易法则";
+            }
+            set { }
+        }
+
+
+    }
+}
