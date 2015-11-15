9c51499136f4b7ba5df692c92b4d68711b33e216
qiujoe <qiujinglei@gmail.com>
2015-11-16 07:44:21 +0800

    增加

---
+++ Stock.Formula/LCD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stock.Formula
+{
+    public class LCD : AbstractFormula, IFormula
+    {
+        private int k;
+        public LCD(int K)
+        {
+            this.k = K;
+        }
+
+        public ValueList Calculate()
+        {
+            ValueList 移均 = MA(CLOSE, 2);
+            ValueList 均价 = EMA(SUM(AMOUNT, 0) / SUM(100 * VOL, 0), 2);
+            ValueList 加速度 = 3000 * MA((均价 - REF(均价, 1)), 2);
+            ValueList 动差 = (移均 - 均价);
+            ValueList 积 = EMA(40 * (加速度 * 动差 * 加速度), 3);
+
+            return null;
+        }
+    }
+}
